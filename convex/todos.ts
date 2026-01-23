ef35438d531d0e6a6bd9094889f8420c9a96e044
rekirxs <rekirx@gmail.com>
2026-01-23 15:33:52 -0500

    finished the UI with the placeholder options of tasks, being able to edit it and delete it

--- convex/todos.ts
+++ convex/todos.ts
@@ -19,12 +19,16 @@ export const addTodo = mutation({
     return todoId;
   },
 });
+
 export const toggleTodo = mutation({
   args: { id: v.id("todos") },
   handler: async (ctx, args) => {
     const todo = await ctx.db.get(args.id);
     if (!todo) throw new ConvexError("Todo not found");
-    await ctx.db.patch(args.id, { IsCompleted: !todo.IsCompleted });
+
+    await ctx.db.patch(args.id, {
+      IsCompleted: !todo.IsCompleted,
+    });
   },
 });
 
@@ -35,19 +39,27 @@ export const deleteTodo = mutation({
   },
 });
 
-export const editTodo = mutation({
-  args: { id: v.id("todos"), text: v.string() },
+export const updateTodo = mutation({
+  args: {
+    id: v.id("todos"),
+    text: v.string(),
+  },
   handler: async (ctx, args) => {
-    await ctx.db.patch(args.id, { text: args.text });
+    await ctx.db.patch(args.id, {
+      text: args.text,
+    });
   },
 });
 
 export const clearAllTodos = mutation({
   handler: async (ctx) => {
     const todos = await ctx.db.query("todos").collect();
+
+    // Delete all todos
     for (const todo of todos) {
       await ctx.db.delete(todo._id);
     }
+
     return { deletedCount: todos.length };
   },
 });
